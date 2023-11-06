959ebb9e501548023cac89cff5fe89a5290c7e59
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2023-11-06 08:13:34 -0300

    guh

--- js/script.js
+++ js/script.js
@@ -4,55 +4,6 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 500,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              arrows: false,
-              slidesToShow: 4,
-              slidesToScroll: 3,
-              infinite: true,
-              
-            }
-          },
-          {
-            breakpoint: 601,
-            settings: {
-              arrows: false,
-              slidesToShow: 4,
-              slidesToScroll: 1
-            }
-          },
-
-
-
-          {
-            breakpoint: 590,
-            settings: {
-              arrows: false,
-              slidesToShow: 2,
-              slidesToScroll: 3,
-              autoplaySpeed: 900,
-              
-             
-              
-            }
-          },
-          {
-            breakpoint: 520,
-            settings: {
-              arrows: false,
-              slidesToShow: 2,
-              slidesToScroll: 3,
-              autoplaySpeed: 900,
-             
-             
-              
-            },
-          },
-        
-          
-        ]
     });
   });
   new WOW().init();
